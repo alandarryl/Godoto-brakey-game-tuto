e136e3fca961eec4db5a7bad63be64e1b18f02d5
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-01-02 21:47:08 +0100

    add an enemy and make it change direction base on the collider

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,7 +1,23 @@
 extends AnimatedSprite2D
 
 const speed = 60
+@onready var enemy = $"."
+@onready var ray_castdonwleft = $RayCastdonwleft
+@onready var ray_castdonwright = $RayCastdonwright
+
+
+var direction = 1
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
 
 func _process(delta):
-	position.x += speed*delta
+	if ray_cast_right.is_colliding() or not ray_castdonwright.is_colliding():
+		direction = -1
+		enemy.flip_h = true
+	if  ray_cast_left.is_colliding() or not ray_castdonwleft.is_colliding():
+		direction = +1
+		enemy.flip_h = false
+	
+	
+	position.x += speed*delta*direction
 	
